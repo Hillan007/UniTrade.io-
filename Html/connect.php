edf7ff6aa5365a20cc1cd3ccdc55104a125c1ab5
Hillan007 <sc21232332023@mut.ac.ke>
2025-03-07 08:35:01 +0300

    new one

--- Html/connect.php
+++ Html/connect.php
@@ -1,11 +1,18 @@
 <?php
 
-$host="localhost";
-$user="root";
-$pass="";
-$db="login";
-$conn=new mysqli($host,$user,$pass,$db);
-if($conn->connect_error){
-    echo "Failed to connect DB".$conn->connect_error;
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "victor";
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Failed to connect to DB: " . $conn->connect_error);
+} else {
+    echo "Connected successfully to the database";
 }
+
 ?>
